b80806756dccaf198aab650871b491554546c6ef
Navan <navanpreet09@gmail.com>
2017-11-21 23:52:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.static(__dirname +'/public'));
 app.use(express.static(__dirname + '/views'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
