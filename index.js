692e23906af602ea86fc7e2ba1a6610b815d08bf
Navan <navanpreet09@gmail.com>
2017-11-21 21:43:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname +'/public'));
 app.use(express.static(__dirname + '/views'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res){
     res.sendFile("index.html");
